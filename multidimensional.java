9b11864000cf61156e987fd179c0caefa3061bd8
sandeepk1909 <sandeepkarumanchi5@gmail.com>
2025-08-26 17:18:44 +0530

    first commit

---
+++ multidimensional.java
@@ -0,0 +1,20 @@
+
+public class multidimensional {
+        public static void main(String[] args) {
+    
+    int [][] arr ={
+        {1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12}
+    };
+    for(int r=0; r<arr.length; r++){
+        for(int c=0; c<arr[r].length;c++){
+            System.out.print(arr[r][c]+" ");
+        }
+        System.out.println();
+    }
+    
+}
+}
+
+
