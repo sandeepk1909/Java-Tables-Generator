e60fb595d55f1a38c7451eebf85616e05792b250
Sandeep <121474685+sandeepk1909@users.noreply.github.com>
2025-09-04 12:31:02 +0530

    Update and rename multidimensional.java to loop.java
    A simple Java program that generates multiplication tables using Scanner for user input. Demonstrates both for loop and while loop approaches.

---
+++ loop.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class loop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        int num =sc.nextInt();
+        for(int i =1; i<=10; i++){
+            System.out.println(num +" * " + i +" = " +(num * i));
+        }
+            System.out.println("Using the while loop here...!");
+    
+        int k =1;
+        while(k<=10){
+        System.out.println(num +" * " + k +" = " +(num * k));
+         k++;
+        }
+        
+        sc.close();
+    }
+}
